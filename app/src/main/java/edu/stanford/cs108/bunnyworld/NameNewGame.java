b0e5bb390fa23fdbf8e63756fdb7eebd6941ead0
Michelle <mdiaz813@stanford.edu>
2018-03-08 14:17:12 -0800

    Name of Game

---
+++ app/src/main/java/edu/stanford/cs108/bunnyworld/NameNewGame.java
@@ -0,0 +1,13 @@
+package edu.stanford.cs108.bunnyworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NameNewGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_new_game);
+    }
+}
